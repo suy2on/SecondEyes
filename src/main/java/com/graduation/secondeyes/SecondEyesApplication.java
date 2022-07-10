ed0ba4a7ef9e8b442f0e658254d8f02cee4277d6
suy2on <tnwjdrms@naver.com>
2022-07-10 19:13:13 +0900

    ✨init

---
+++ src/main/java/com/graduation/secondeyes/SecondEyesApplication.java
@@ -0,0 +1,13 @@
+package com.graduation.secondeyes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondEyesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondEyesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/graduation/secondeyes/SecondEyesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graduation.secondeyes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondEyesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
