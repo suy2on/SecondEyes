8dfd8f307b0408a01485158ce5fb3a7133eff092
suy2on <tnwjdrms@naver.com>
2022-07-10 19:13:44 +0900

    ✨docker 연결테스트진행

---
+++ src/main/java/com/graduation/secondeyes/Sample.java
@@ -0,0 +1,15 @@
+package com.graduation.secondeyes;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Sample {
+
+    @RequestMapping("/sample")
+    public String greeting(){
+        return "sample!!";
+    }
+
+}
